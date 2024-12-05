37130cac8562790f0f2ab21b5eb0496c8b7efadc
annahutsaliuk <hannah.htslk@gmail.com>
2024-12-05 19:11:01 +0200

    add img to about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import s from './AboutPage.module.css';
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className={s.container}>
+      <img
+        height="573px"
+        src="../../../src/assets/AboutUs/about.png"
+        alt="about_us"
+      />
+    </div>
+  );
 };
 
 export default AboutPage;
